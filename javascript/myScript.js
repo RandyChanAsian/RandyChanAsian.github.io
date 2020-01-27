abe06ad88cd80ba925537fc52d3a1a3c7dd629f9
RandyChanAsian <55281119+RandyChanAsian@users.noreply.github.com>
2020-01-27 17:38:35 -0500

    Update myScript.js

--- javascript/myScript.js
+++ javascript/myScript.js
@@ -2,3 +2,10 @@ for(x=1; x<9; x++) {
   var ans = Math.pow(2,x);
   document.write(ans + "<br/>");
 }
+function start(){
+	var name = readLine("What do you use? Apple or Samsung? ");
+	println("Name is: " + name);
+	
+	var age = readInt("What is your age? ");
+	println("Age is: " + age);
+}
