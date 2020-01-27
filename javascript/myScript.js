99059a65ff5ddd071f98d582665d479057186081
RandyChanAsian <55281119+RandyChanAsian@users.noreply.github.com>
2020-01-27 17:42:54 -0500

    Update myScript.js

--- javascript/myScript.js
+++ javascript/myScript.js
@@ -1,7 +1,3 @@
-for(x=1; x<9; x++) {
-  var ans = Math.pow(2,x);
-  document.write(ans + "<br/>");
-}
 function start(){
 	var name = readLine("What do you use? Apple or Samsung? ");
 	println("Name is: " + name);
